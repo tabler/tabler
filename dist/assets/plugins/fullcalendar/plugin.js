0f316de5ef7b9865420a42fa656e0a94808486f4
codecalm <codecalm@gmail.com>
2018-02-14 22:04:13 +0100

    dist

---
+++ dist/assets/plugins/fullcalendar/plugin.js
@@ -0,0 +1,9 @@
+require.config({
+    shim: {
+        'fullcalendar': ['moment', 'jquery'],
+    },
+    paths: {
+        'fullcalendar': 'assets/plugins/fullcalendar/js/fullcalendar.min',
+        'moment': 'assets/plugins/fullcalendar/js/moment.min',
+    }
+});
---
+++ dist/assets/plugins/prismjs/plugin.js
@@ -0,0 +1,18 @@
+require.config({
+    paths: {
+        'prismjs': 'assets/plugins/prismjs/js/prism.pack',
+    },
+	shim: {
+		prism: {
+			exports: "Prism"
+		}
+	}
+});
+
+require(['prismjs', 'jquery'], function(prismjs, $){
+    $(document).ready(function(){
+        // $('[class^="language-"]').each(function(i, block) {
+	     //    Prism.highlightElement(block);
+        // });
+    });
+});
