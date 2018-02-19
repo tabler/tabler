5597250d7b604cba000ec12bb8ed278269bc423e
codecalm <codecalm@gmail.com>
2018-02-19 20:17:02 +0100

    jekyll dirs

--- dist/assets/plugins/fullcalendar/plugin.js
+++
@@ -1,9 +0,0 @@
-require.config({
-    shim: {
-        'fullcalendar': ['moment', 'jquery'],
-    },
-    paths: {
-        'fullcalendar': 'assets/plugins/fullcalendar/js/fullcalendar.min',
-        'moment': 'assets/plugins/fullcalendar/js/moment.min',
-    }
-});
