8b87199fb51624e9affdb5ca00b7129b3c6a6e58
chomik <codecalm@gmail.com>
2018-02-26 16:07:17 +0100

    gh-pages script

---
+++ gh-pages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e # stop on error
+
+echo "check out branch gh-pages ..."
+git checkout -b gh-pages
+echo "add dist folder"
+git add -f dist
+echo "commit changes"
+git commit -m "deploy to gh-pages"
+echo "push to remote gh-pages"
+git push origin `git subtree split --prefix dist`:gh-pages --force
+echo "checkout branch master"
+git checkout master
+echo "delete branch gh-pages"
+git branch -D gh-pages
+echo "All done!"
