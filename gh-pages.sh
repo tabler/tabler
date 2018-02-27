3d4e2737ec1d5a77ba3c6c7dff7e0dda4ac78ed9
codecalm <codecalm@gmail.com>
2018-02-27 21:46:19 +0100

    gh pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 set -e # stop on error
 
+git checkout master
+
+exists=`git show-ref refs/heads/gh-pages`
+if [ -n "$exists" ]; then
+    git branch -D gh-pages
+fi
+
 echo "check out branch gh-pages ..."
 git checkout -b gh-pages
+
 echo "add dist folder"
 git add -f dist
+
 echo "commit changes"
 git commit -m "deploy to gh-pages"
+
 echo "push to remote gh-pages"
 git push origin `git subtree split --prefix dist`:gh-pages --force
+
 echo "checkout branch master"
 git checkout master
+
 echo "delete branch gh-pages"
 git branch -D gh-pages
+
 echo "All done!"
