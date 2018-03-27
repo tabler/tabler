8bf83f3cd44d1f6c2f97733e95286402514b83cb
chomik <codecalm@gmail.com>
2018-03-27 11:39:05 +0200

    version 0.0.13

--- dist-new-version.sh
+++ dist-new-version.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-version=0.0.12
+version=0.0.13
 
 echo "Version $version"
 
 npm run dist
 git add .
 git commit -am "version $version"
+git tag "v$version"
 git push origin master
 
 npm run publish-dist
