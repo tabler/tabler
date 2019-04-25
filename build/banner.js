880ff15961261ff95f123882bf85b6f074cecc40
chomik <codecalm@gmail.com>
2019-04-25 09:08:16 +0200

    css lint, css init, build process

---
+++ build/banner.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const pkg = require('../package.json')
+const year = new Date().getFullYear()
+
+function getBanner(pluginFilename) {
+  return `/*!
+  * Tabler${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
+  * Copyright 2018-${year} ${pkg.author}
+  * Licensed under MIT (https://github.com/twbs/tabler/blob/master/LICENSE)
+  */`
+}
+
+module.exports = getBanner
---
+++ build/rollup.config.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const path = require('path')
+const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+const banner = require('./banner.js')
+
+const BUNDLE = process.env.BUNDLE === 'true'
+const ESM = process.env.ESM === 'true'
+
+let fileDest = `tabler${ESM ? '.esm' : ''}`
+const external = ['popper.js']
+const plugins = [
+  babel({
+    // Only transpile our source code
+    exclude: 'node_modules/**',
+    // Include only required helpers
+    externalHelpersWhitelist: [
+      'defineProperties',
+      'createClass',
+      'inheritsLoose',
+      'defineProperty',
+      'objectSpread'
+    ]
+  })
+]
+const globals = {
+  'popper.js': 'Popper'
+}
+
+if (BUNDLE) {
+  fileDest += '.bundle'
+  // Remove last entry in external array to bundle Popper
+  external.pop()
+  delete globals['popper.js']
+  plugins.push(resolve())
+}
+
+const rollupConfig = {
+  input: path.resolve(__dirname, `../js/tabler.js`),
+  output: {
+    banner,
+    file: path.resolve(__dirname, `../dist/js/${fileDest}.js`),
+    format: ESM ? 'esm' : 'umd',
+    globals
+  },
+  external,
+  plugins
+}
+
+if (!ESM) {
+  rollupConfig.output.name = 'tabler'
+}
+
+module.exports = rollupConfig
