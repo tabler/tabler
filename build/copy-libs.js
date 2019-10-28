61c59e8859e6669bd984d7b45bdff01ed6863959
codecalm <codecalm@gmail.com>
2019-10-28 23:15:07 +0100

    libs `@` fix

--- build/copy-libs.js
+++ build/copy-libs.js
@@ -5,7 +5,7 @@ const libs = require('../pages/_data/libs'),
 const all_libs = libs.js.concat(libs.css);
 
 all_libs.forEach(function (lib) {
-  let dirname = path.dirname(lib);
-  let cmd = `mkdir -p "dist/libs/${dirname}" && cp -r node_modules/${lib} dist/libs/${lib}`;
+  let dirname = path.dirname(lib).replace('@', '');
+  let cmd = `mkdir -p "dist/libs/${dirname}" && cp -r node_modules/${lib} dist/libs/${lib.replace('@', '')}`;
   exec(cmd)
 });
