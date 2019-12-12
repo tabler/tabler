f37388d8be7bc00d00e0ff96cf4ad09c79788c90
Dawid <david03h@wp.pl>
2019-12-12 08:19:43 +0100

    nouislider added

--- js/tabler.js
+++ js/tabler.js
@@ -118,6 +118,20 @@ $(document).ready(function() {
   // $('[data-toggle="tooltip"]').tooltip();
   // $('[data-toggle="popover"]').popover();
 
+/* NoUiSlider */
+
+if(window.noUiSlider){
+  let boi = document.getElementById("slider");
+  noUiSlider.create(boi,{
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+  });
+}
+
   /*
   Autosize plugin
    */
