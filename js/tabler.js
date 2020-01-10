9ff68e407df50d5a6da2ec27e1b48fa49a48e226
codecalm <codecalm@gmail.com>
2020-01-10 22:53:31 +0100

    popover js fixes

--- js/tabler.js
+++ js/tabler.js
@@ -47,8 +47,8 @@ $(document).ready(function () {
 	/*
 	Popover
 	 */
-	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
-	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'));
+	popoverTriggerList.map(function (popoverTriggerEl) {
 		return new bootstrap.Popover(popoverTriggerEl, {
 			autoHide: true
 		})
