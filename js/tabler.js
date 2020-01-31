ac225344e5cc660a5a82e7a12a9e382c7473107b
Dawid Harat <dawidharat03@gmail.com>
2020-01-31 19:07:12 +0100

    countup fix

--- js/tabler.js
+++ js/tabler.js
@@ -2,7 +2,7 @@
 
 import {Dropdown, Tooltip, Popover} from 'bootstrap';
 import 'popper.js';
-// import {CountUp} from "countup.js";
+import {CountUp} from "countup.js";
 
 (function () {
 	/**
@@ -34,9 +34,10 @@ import 'popper.js';
 	/*
 	CountUp
 	*/
-	// let countupTriggerList = [].slice.call(document.querySelectorAll("[data-countup]"));
-	// countupTriggerList.map(function (countupTriggerEl) {
-	// 	let dataCountUp = JSON.parse(countupTriggerEl.getAttribute("data-countup"));
-	// 	return (new CountUp(countupTriggerEl, parseFloat(countupTriggerEl.innerText), dataCountUp)).start();
-	// });
+	let countupTriggerList = [].slice.call(document.querySelectorAll("[data-countup]"));
+	countupTriggerList.map(function (countupTriggerEl) {
+		let dataCountUp;
+		if(countupTriggerEl.getAttribute("data-countup") != "") dataCountUp = JSON.parse(countupTriggerEl.getAttribute("data-countup"));
+		return (new CountUp(countupTriggerEl, parseFloat(countupTriggerEl.innerText), dataCountUp)).start();
+	});
 })();
