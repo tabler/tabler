9f820f6b73432825b20f37aff313e18ef65f830c
codecalm <codecalm@gmail.com>
2020-02-22 12:38:47 +0100

    version update

--- build/scss-compile.js
+++ build/scss-compile.js
@@ -1,10 +1,3 @@
-/*
- * Tabler (v0.9.0): scss-compile.js
- * Copyright 2018-2019 The Tabler Authors
- * Copyright 2018-2019 codecalm
- * Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
- */
-
 const BUNDLE = process.env.BUNDLE === 'true';
 
 const path = require('path'),
