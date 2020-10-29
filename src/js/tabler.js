e99982daf92f3a1b3c0d19a3983d9503726e7e3a
codecalm <codecalm@gmail.com>
2020-10-29 23:38:55 +0100

    bootstrap upgrade, js fix

--- src/js/tabler.js
+++ src/js/tabler.js
@@ -1,27 +1,34 @@
 //Vendor
-import 'bootstrap';
+// import 'bootstrap';
 
-/**
- */
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
-tooltipTriggerList.map(function (tooltipTriggerEl) {
-	let options = {
-		delay: {show: 50, hide: 50},
-		html: true,
-		placement: 'auto'
-	};
-	return new bootstrap.Tooltip(tooltipTriggerEl, options);
-});
+(function() {
+	/**
+	 */
+	let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+	tooltipTriggerList.map(function (tooltipTriggerEl) {
+		let options = {
+			delay: {show: 50, hide: 50},
+			html: true,
+			placement: 'auto'
+		};
+		return new bootstrap.Tooltip(tooltipTriggerEl, options);
+	});
 
-/**
- */
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'));
-popoverTriggerList.map(function (popoverTriggerEl) {
-	let options = {
-		delay: {show: 50, hide: 50},
-		html: true,
-		placement: 'auto'
-	};
-	return new bootstrap.Popover(popoverTriggerEl, options);
-});
+	/**
+	 */
+	let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'));
+	popoverTriggerList.map(function (popoverTriggerEl) {
+		let options = {
+			delay: {show: 50, hide: 50},
+			html: true,
+			placement: 'auto'
+		};
+		return new bootstrap.Popover(popoverTriggerEl, options);
+	});
 
+	let dropdownTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="dropdown"]'));
+	dropdownTriggerList.map(function (dropdownTriggerEl) {
+		return new bootstrap.Dropdown(dropdownTriggerEl);
+	});
+
+})();
