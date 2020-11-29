d2e8d39ad182dc60f70b8dbb9c8e81d0a93091d2
codecalm <codecalm@gmail.com>
2020-11-29 15:32:43 +0100

    Release 1.0.0-alpha.15

--- demo/dist/js/tabler.js
+++ demo/dist/js/tabler.js
@@ -1,6 +1,6 @@
 /*!
-* Tabler v1.0.0-alpha.14 (https://tabler.io)
-* @version 1.0.0-alpha.14
+* Tabler v1.0.0-alpha.15 (https://tabler.io)
+* @version 1.0.0-alpha.15
 * @link https://tabler.io
 * Copyright 2018-2020 The Tabler Authors
 * Copyright 2018-2020 codecalm.net Paweł Kuna
