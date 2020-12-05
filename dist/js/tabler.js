eb93e6d91821f7d583e3b0df768c7c7b8e4f2722
codecalm <codecalm@gmail.com>
2020-12-05 21:34:42 +0100

    homepage charts fixesbootstrap upgrade

--- dist/js/tabler.js
+++ dist/js/tabler.js
@@ -13,26 +13,17 @@
 
 	var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
-	function createCommonjsModule(fn, basedir, module) {
-		return module = {
-			path: basedir,
-			exports: {},
-			require: function (path, base) {
-				return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-			}
-		}, fn(module, module.exports), module.exports;
+	function createCommonjsModule(fn) {
+	  var module = { exports: {} };
+		return fn(module, module.exports), module.exports;
 	}
 
-	function commonjsRequire () {
-		throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
-	}
-
-	var autosize = createCommonjsModule(function (module, exports) {
 	/*!
 		autosize 4.0.2
 		license: MIT
 		http://www.jacklmoore.com/autosize
 	*/
+	var autosize = createCommonjsModule(function (module, exports) {
 	(function (global, factory) {
 		{
 			factory(module, exports);
