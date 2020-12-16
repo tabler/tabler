f3be0c14d969e2064a446ed410e0ec197c5a4f4b
codecalm <codecalm@gmail.com>
2020-12-16 13:03:40 +0100

    Release 1.0.0-alpha.18

--- dist/css/demo.css
+++ dist/css/demo.css
@@ -1,6 +1,6 @@
 /*!
-* Tabler v1.0.0-alpha.17 (https://tabler.io)
-* @version 1.0.0-alpha.17
+* Tabler v1.0.0-alpha.18 (https://tabler.io)
+* @version 1.0.0-alpha.18
 * @link https://tabler.io
 * Copyright 2018-2020 The Tabler Authors
 * Copyright 2018-2020 codecalm.net Paweł Kuna
