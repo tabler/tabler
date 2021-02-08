9f107202de0c5a8df945146464ef1385a123294c
codecalm <codecalm@gmail.com>
2021-02-09 00:50:24 +0100

    new datepicker

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,5 +1,17 @@
-// Nested dropdowns
+import { Dropdown } from 'bootstrap';
 
+/*
+Core dropdowns
+ */
+let dropdownTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
+dropdownTriggerList.map(function (dropdownTriggerEl) {
+	return new Dropdown(dropdownTriggerEl);
+});
+
+
+/*
+Nested dropdowns
+ */
 const selectors = '.dropdown, .dropup, .dropend, .dropstart',
 	dropdowns = document.querySelectorAll(selectors);
 
--- src/js/tabler.js
+++ src/js/tabler.js
@@ -1,54 +1,10 @@
 //Vendor
 
-import 'bootstrap/dist/js/bootstrap.bundle';
-
 import './autosize';
 import './input-mask';
 import './dropdown';
+import './tooltip';
+import './popover';
+import './switch-icon';
+import './toast';
 
-(function() {
-	/**
-	 */
-	let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-	tooltipTriggerList.map(function (tooltipTriggerEl) {
-		let options = {
-			delay: {show: 50, hide: 50},
-			html: tooltipTriggerEl.getAttribute("data-bs-html") === "true" ?? false,
-			placement: tooltipTriggerEl.getAttribute('data-bs-placement') ?? 'auto'
-		};
-		return new bootstrap.Tooltip(tooltipTriggerEl, options);
-	});
-
-	/**
-	 */
-	let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-	popoverTriggerList.map(function (popoverTriggerEl) {
-		let options = {
-			delay: {show: 50, hide: 50},
-			html: popoverTriggerEl.getAttribute('data-bs-html') === "true" ?? false,
-			placement: popoverTriggerEl.getAttribute('data-bs-placement') ?? 'auto'
-		};
-		return new bootstrap.Popover(popoverTriggerEl, options);
-	});
-
-	let dropdownTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
-	dropdownTriggerList.map(function (dropdownTriggerEl) {
-		return new bootstrap.Dropdown(dropdownTriggerEl);
-	});
-
-
-	let switchesTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="switch-icon"]'));
-	switchesTriggerList.map(function (switchTriggerEl) {
-		switchTriggerEl.addEventListener('click', (e) => {
-			e.stopPropagation();
-
-			switchTriggerEl.classList.toggle('active');
-		});
-	});
-
-	let toastsTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="toast"]'));
-	toastsTriggerList.map(function (toastTriggerEl) {
-		return new bootstrap.Toast(toastTriggerEl);
-	});
-
-})();
