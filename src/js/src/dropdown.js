38e737570269d2fef7509f73aec78be6b65c71a0
Michal Wolny <michal.wolny@versum.pl>
2021-05-09 21:08:11 +0200

    remove console log

--- src/js/src/dropdown.js
+++ src/js/src/dropdown.js
@@ -5,6 +5,5 @@ Core dropdowns
  */
 let dropdownTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
 dropdownTriggerList.map(function (dropdownTriggerEl) {
-	console.log(dropdownTriggerEl.dataset);
 	return new Dropdown(dropdownTriggerEl);
 });
