106acad15f1c09d21df5e39f165cb706d350f058
codecalm <codecalm@gmail.com>
2021-11-02 23:13:44 +0100

    ui changes test

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -6,12 +6,12 @@ const Template = ({ label, ...args }) => {
   return `<button class="btn">${label}</button>`;
 };
 
-export const Primary = () => `<button class="btn btn-primary">Button</button>`;
+export const Primary = () => `<button class="btn btn-primary">Primary button</button>`;
 
-export const Secondary = () => `<button class="btn">Button</button>`;
+export const Secondary = () => `<button class="btn">Secondary button</button>`;
 
-export const Danger = () => `<button class="btn btn-danger">Button</button>`;
+export const Danger = () => `<button class="btn btn-danger">Danger button</button>`;
 
-export const Large = () => `<button class="btn btn-lg">Button</button>`;
+export const Large = () => `<button class="btn btn-lg">Large button</button>`;
 
-export const Small = () => `<button class="btn btn-sm">Button</button>`;
+export const Small = () => `<button class="btn btn-sm">Small button</button>`;
