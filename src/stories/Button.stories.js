801d5c31970896dbb323641eb248ce7bf2133f10
codecalm <codecalm@gmail.com>
2021-11-02 23:10:08 +0100

    danger button test

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -10,6 +10,8 @@ export const Primary = () => `<button class="btn btn-primary">Button</button>`;
 
 export const Secondary = () => `<button class="btn">Button</button>`;
 
+export const Danger = () => `<button class="btn btn-danger">Button</button>`;
+
 export const Large = () => `<button class="btn btn-lg">Button</button>`;
 
 export const Small = () => `<button class="btn btn-sm">Button</button>`;
