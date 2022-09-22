09a291f14497e099679a5c96c38b4d45b4859cf0
Santanu Barman <106900914+Santanu-dev@users.noreply.github.com>
2022-09-22 08:38:52 +0530

    corrected options spelling

--- src/js/src/dropdown.js
+++ src/js/src/dropdown.js
@@ -5,7 +5,7 @@ Core dropdowns
  */
 let dropdownTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
 dropdownTriggerList.map(function (dropdownTriggerEl) {
-	let oprions = {
+	let options = {
 		boundary: dropdownTriggerEl.getAttribute('data-bs-boundary') === 'viewport' ? document.querySelector('.btn') : 'clippingParents',
 	}
 	return new Dropdown(dropdownTriggerEl, oprions);
