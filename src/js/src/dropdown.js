07b0292e2c1038632fe33c4c858c346cfd002dbc
Santanu Barman <106900914+Santanu-dev@users.noreply.github.com>
2022-09-27 01:43:25 +0530

    fixed dropdown issue on tables issue #1249 (#1269)

--- src/js/src/dropdown.js
+++ src/js/src/dropdown.js
@@ -5,5 +5,8 @@ Core dropdowns
  */
 let dropdownTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
 dropdownTriggerList.map(function (dropdownTriggerEl) {
-	return new Dropdown(dropdownTriggerEl);
+	let options = {
+		boundary: dropdownTriggerEl.getAttribute('data-bs-boundary') === 'viewport' ? document.querySelector('.btn') : 'clippingParents',
+	}
+	return new Dropdown(dropdownTriggerEl, options);
 });
