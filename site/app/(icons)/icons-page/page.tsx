1924c11ee7fbe71659eadcddcd0325eebcbe3561
Bartłomiej Gawęda <bgaweda@abis.krakow.pl>
2023-08-19 18:15:58 +0200

    Add list of icons on icons-page
    TODO: Change size of icons.
    TODO: Add AOS.

--- site/app/(icons)/icons-page/page.tsx
+++ site/app/(icons)/icons-page/page.tsx
@@ -1,7 +1,32 @@
-export default function IconsPage() {
+import { icons } from '@/config/tabler';
+import IconSvg from "@/components/IconSvg"
+
+const IconsList = () => {
   return (
-    <section className="section">
-      <div className="container">123</div>
+    <section className="section section-light">
+      <div className="container">
+
+        <div className="row" data-aos-id-icons>
+          {Object.values(icons).map((icon, i) => (
+            <div className="sm:col-4 md:col-3 lg:col-2" key={icon.name}>
+              {/*TODO AOS data-aos="fade-up" data-aos-anchor="[data-aos-id-icons]"*/}
+              <span className="icon-card text-centered">
+                <IconSvg svg={icon.svg} key={icon.name} className="icon-md" />
+                {/*TODO Change size of icon*/}
+                <div className="icon-name">{icon.name}</div>
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
+}
+
+export default function IconsPage() {
+  return (
+    <>
+      <IconsList />
+    </>
+  )
 }
