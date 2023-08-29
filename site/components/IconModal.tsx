e4a4a1ceb02d9e701653ad3d13584d72e2205061
Bartłomiej Gawęda <bagaweda@gmail.com>
2023-08-29 18:51:41 +0200

    Add names to copy & tags in IconModal
    + Fix closing modal on backdrop click.

--- site/components/IconModal.tsx
+++ site/components/IconModal.tsx
@@ -1,15 +1,39 @@
 import IconSvg from '@/components/IconSvg';
+import { useClipboard } from '@/hooks';
 
-const IconModal = ({name, category, tags, svg, unicode}) => (
-  <div className="modal-content row">
-    <div className="md:col-4">
-      <IconSvg svg={svg} key={name} className="icon-lg"/>
-    </div>
-    <div className="md:col-8 icon-details">
-      <h2>{name}</h2>
+const IconModal = ({ name, category, tags, svg, unicode }) => {
+  const clipboard = useClipboard();
 
+  const subNames = [
+    { name: 'Icon' + name.replace('-', ''), tooltip: 'Copy React name' }, // TODO How to get React name?
+    { name: '&#x' + unicode + ';', tooltip: 'Copy HTML char' },
+    { name: unicode, tooltip: 'Copy hex' },
+  ];
+
+  return (
+    <div className="modal-content row">
+      <div className="md:col-4">
+        <IconSvg svg={svg} key={name} className="icon-lg" />
+      </div>
+      <div className="md:col-8 icon-details">
+        <h2>{name}</h2>
+        <div className="list-inline-dots">
+          {subNames.map((subName) => (
+            <div key={subName.name}>
+              <a onClick={() => clipboard.copy(subName.name)} className="tooltip" data-title={subName.tooltip}>
+                {subName.name}
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className="tags-list">
+          {tags.map((tag) => (
+            <span key={tag} className="tag tag-sm">{tag}</span>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default IconModal;
--- site/components/Modal2.tsx
+++ site/components/Modal2.tsx
@@ -7,16 +7,17 @@ const Modal = ({ content }) => {
   let { showModal, handleModal } = useContext(ModalContext);
   // const modalRoot = document.getElementById('#modal-root');
   return showModal ? (
-        <div className="modal-backdrop" tabIndex={-1} role="dialog" onClick={handleModal} onKeyDown={handleModal}>
-          <div className="modal">
-            <a href="#" className="modal-close" data-dismiss="modal" aria-label="Close modal" onClick={handleModal}>
-              <Icon name="x" />
-            </a>
-            <div className="modal-body">{content}</div>
-          </div>
-        </div>
-      )
-    : <></>;
+    <div className="modal-backdrop" tabIndex={-1} role="dialog" onClick={handleModal} onKeyDown={handleModal}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <a href="#" className="modal-close" data-dismiss="modal" aria-label="Close modal" onClick={handleModal}>
+          <Icon name="x" />
+        </a>
+        <div className="modal-body">{content}</div>
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default Modal;
