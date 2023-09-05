ab21945f90acb987f48756c3fa9f5d344c5d31e7
Bartłomiej Gawęda <bgaweda@abis.krakow.pl>
2023-09-05 21:31:12 +0200

    Use NEXT_PUBLIC_VERCEL_URL from environment

--- site/data/icons-api.ts
+++ site/data/icons-api.ts
@@ -5,7 +5,7 @@ export const getIcons = async (
   category: string = 'all',
   limit: number = Infinity,
 ): Promise<{ limit: number; icons: IconsType }> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/icons?limit=${limit}&category=${category}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/icons?limit=${limit}&category=${category}`, {
     method: 'GET',
   });
   return response.json();
