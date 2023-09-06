67e92c5c657292bc5daa04beb846427155e67ef7
codecalm <codecalm@gmail.com>
2023-09-06 20:30:24 +0200

    build fix

--- site/app/api/icons/route.ts
+++ site/app/api/icons/route.ts
@@ -2,6 +2,8 @@ import { icons } from '@/config/tabler';
 import { IconsType } from '@/types';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
