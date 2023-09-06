1f39cad449742543b9e989efe84f118bf7a155b9
Bartłomiej Gawęda <bgaweda@abis.krakow.pl>
2023-09-06 17:50:45 +0200

    Add try-catch in GET icons route

--- site/app/api/icons/route.ts
+++ site/app/api/icons/route.ts
@@ -3,14 +3,23 @@ import { IconsType } from '@/types';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const limit = +(searchParams.get('limit') || Infinity);
-  const category = (searchParams.get('category') || 'all').toLowerCase();
+  try {
+    const { searchParams } = new URL(request.url);
+    const limit = +(searchParams.get('limit') || Infinity);
+    const category = (searchParams.get('category') || 'all').toLowerCase();
 
-  return NextResponse.json({
-    limit,
-    icons: Object.values(icons)
-      .filter((icon) => category === 'all' || icon.category.toLowerCase() === category)
-      .slice(0, limit) as IconsType,
-  });
+    return NextResponse.json({
+      limit,
+      icons: Object.values(icons)
+        .filter((icon) => category === 'all' || icon.category.toLowerCase() === category)
+        .slice(0, limit) as IconsType,
+    });
+  } catch (error) {
+    console.log('Error inside GET route', error);
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 });
+    }
+
+    return new Response('Internal server error', { status: 500 });
+  }
 }
