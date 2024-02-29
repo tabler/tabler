3632b6d0aca54445d9a3370fb8c04cac50c9a213
codecalm <codecalm@gmail.com>
2024-02-29 01:41:47 +0100

    env variables

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (config) {
+	config.addWatchTarget("./dist/css/*");
+	config.addWatchTarget("./dist/js/*");
+	config.addWatchTarget("./scss/*");
+
 	config.addPassthroughCopy("dist");
-	config.addWatchTarget("dist/css");
-	config.addWatchTarget("dist/js");
+
+	config.addGlobalData('env', process.env.ELEVENTY_ENV || "development");
 
 	const filters = ["timeago", "random_date_ago", "replace_regex", "first_letters", "timestamp_to_date", "random_number", "random_date", "markdownify", "random_item", "extract", "format_number", "divide", "number_color", "svg_icon", "hex_to_rgb", "concat_objects", "seconds_to_minutes", "miliseconds_to_minutes", "htmlbeautifier", "hex_to_rgb", "random_id", "date_to_string"]
 	filters.forEach((filter) => {
