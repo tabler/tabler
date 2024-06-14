63a233cd645dfb809b45038da198885166ca09c4
codecalm <codecalm@gmail.com>
2024-06-14 17:13:02 +0200

    remove `demo` and `dist` folder from repo

--- demo/dist/libs/tinymce/plugins/template/index.js
+++
@@ -1,7 +0,0 @@
-// Exports the "template" plugin for usage with module loaders
-// Usage:
-//   CommonJS:
-//     require('tinymce/plugins/template')
-//   ES2015:
-//     import 'tinymce/plugins/template'
-require('./plugin.js');
