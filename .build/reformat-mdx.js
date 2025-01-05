2698d3ab42eced381455864cec913214855dd4b8
codecalm <codecalm@gmail.com>
2025-01-05 22:51:03 +0100

    Enhance HTML formatting in markdown files using js-beautify

--- .build/reformat-mdx.js
+++ .build/reformat-mdx.js
@@ -5,7 +5,7 @@
 const fs = require('fs'),
 	path = require('path'),
 	glob = require('glob'),
-	prettier = require('prettier');
+	beautifyHtml = require('js-beautify').html;
 
 const docs = glob
 	.sync(path.join(__dirname, `../docs/**/*.mdx`))
@@ -14,14 +14,19 @@ docs.forEach((file, i) => {
 	const oldContent = fs.readFileSync(file, 'utf8')
 
 	// get codeblocks from markdown
-	const content = oldContent.replace(/(```.*?\n)(.*?)(```)/gs, (m, m1, m2, m3) => {
-		const m2m = prettier.format(m2, {
-			semi: false,
-			singleQuote: true,
-			// parser: 'babel'
-		}).trim();
+	const content = oldContent.replace(/(```([a-z0-9]+).*?\n)(.*?)(```)/gs, (m, m1, m2, m3, m4) => {
+		if (m2 === 'html') {
+			const m3m = beautifyHtml(m3, {
+				"indent_size": 2,
+				"indent_char": " ",
+			}).trim();
 
-		return m1 + m2m + "\n" + m3;
+			return m1 + m3m + "\n" + m4;
+		}
+
+		return m
+
+		
 	})
 
 	if (content !== oldContent) {
