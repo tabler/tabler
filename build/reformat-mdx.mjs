f06cce0300c78266577ababb9c0b6956db57b58d
codecalm <codecalm@gmail.com>
2025-02-26 19:20:37 +0100

    Clean up HTML syntax and remove unused dependencies in documentation and package.json

--- build/reformat-mdx.mjs
+++ build/reformat-mdx.mjs
@@ -6,33 +6,60 @@ import { readFileSync, writeFileSync } from 'node:fs';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url'
 import { sync } from 'glob';
-import beautify from 'js-beautify';
+import * as prettier from "prettier";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const docs = sync(join(__dirname, '..', 'docs', '**', '*.mdx'))
 
-docs.forEach((file, i) => {
+async function formatHTML(htmlString) {
+	try {
+		const formattedHtml = await prettier.format(htmlString, {
+			parser: "html",
+			printWidth: 100,
+		});
+		return formattedHtml;
+	} catch (error) {
+		console.error("Error formatting HTML:", error);
+		return htmlString; // Return original in case of an error
+	}
+}
+
+async function replaceAsync(str, regex, asyncFn) {
+	const matches = [...str.matchAll(regex)];
+
+	const replacements = await Promise.all(
+		matches.map(async (match) => asyncFn(...match))
+	);
+
+	let result = str;
+	matches.forEach((match, i) => {
+		result = result.replace(match[0], replacements[i]);
+	});
+
+	return result;
+}
+
+for (const file of docs) {
 	const oldContent = readFileSync(file, 'utf8')
 
 	// get codeblocks from markdown
-	const content = oldContent.replace(/(```([a-z0-9]+).*?\n)(.*?)(```)/gs, (m, m1, m2, m3, m4) => {
+	const content = await replaceAsync(oldContent, /(```([a-z0-9]+).*?\n)(.*?)(```)/gs, async (m, m1, m2, m3, m4) => {
 		if (m2 === 'html') {
-			// m3 = beautify.default.html(m3, {
-			// 	"indent_size": 2,
-			// 	"indent_char": " ",
-			// }).trim();
+			m3 = await formatHTML(m3);
+
+			console.log(m3);
 
 			// remove empty lines
 			m3 = m3.replace(/^\s*[\r\n]/gm, '');
 
-			return m1 + m3 + "\n" + m4;
+			return m1 + m3.trim() + "\n" + m4;
 		}
-		return m
+		return m.trim();
 	})
 
 	if (content !== oldContent) {
 		writeFileSync(file, content, 'utf8')
 		console.log(`Reformatted ${file}`)
 	}
-})
+}
