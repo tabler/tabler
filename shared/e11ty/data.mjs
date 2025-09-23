dbb5e7d2edf040d9a775c4f23266585cfd406feb
codecalm <codecalm@gmail.com>
2025-09-23 23:07:40 +0200

    fix: restore analytics environment variables in appData

--- shared/e11ty/data.mjs
+++ shared/e11ty/data.mjs
@@ -5,4 +5,8 @@ export function appData(eleventyConfig) {
 	eleventyConfig.addGlobalData("package", JSON.parse(readFileSync(join("..", "core", "package.json"), "utf-8")));
 	eleventyConfig.addGlobalData("changelog", readFileSync(join("..", "core", "CHANGELOG.md"), "utf-8"));
 	eleventyConfig.addGlobalData("libs", JSON.parse(readFileSync(join("..", "core", "libs.json"), "utf-8")));
+
+	// Analytics Environment Variables
+	eleventyConfig.addGlobalData("posthogApiKey", process.env.NEXT_PUBLIC_POSTHOG_KEY);
+	eleventyConfig.addGlobalData("posthogHost", process.env.NEXT_PUBLIC_POSTHOG_HOST);
 }
