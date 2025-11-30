f90b7547fbdd7e2efccfb8e9fbf2dde1937113fc
codecalm <codecalm@gmail.com>
2025-11-30 21:37:59 +0100

    refactor: Update SCSS variables for alerts and avatars, enhance card and progress background styles

--- shared/e11ty/filters.mjs
+++ shared/e11ty/filters.mjs
@@ -160,31 +160,36 @@ export function appFilters(eleventyConfig) {
 
 	// time ago from today
 	eleventyConfig.addFilter("timeago", function (date) {
-		const seconds = Math.floor((new Date() - date) / 1000);
+		let seconds;
+		if (typeof date === 'number') {
+			seconds = date;
+		} else {
+			seconds = Math.floor((new Date() - date) / 1000);
+		}
 
 		let interval = Math.floor(seconds / 31536000);
 
-		if (interval > 1) {
-			return interval + " years ago";
+		if (interval >= 1) {
+			return `${interval} year${interval > 1 ? 's' : ''} ago`;
 		}
 		interval = Math.floor(seconds / 2592000);
-		if (interval > 1) {
-			return interval + " months ago";
+		if (interval >= 1) {
+			return `${interval} month${interval > 1 ? 's' : ''} ago`;
 		}
 		interval = Math.floor(seconds / 86400);
-		if (interval > 1) {
-			return interval + " days ago";
+		if (interval >= 1) {
+			return `${interval} day${interval > 1 ? 's' : ''} ago`;
 		}
 		interval = Math.floor(seconds / 3600);
-		if (interval > 1) {
-			return interval + " hours ago";
+		if (interval >= 1) {
+			return `${interval} hour${interval > 1 ? 's' : ''} ago`;
 		}
 		interval = Math.floor(seconds / 60);
-		if (interval > 1) {
-			return interval + " minutes ago";
+		if (interval >= 1) {
+			return `${interval} minute${interval > 1 ? 's' : ''} ago`;
 		}
 		if (seconds > 0) {
-			return Math.floor(seconds) + " seconds ago";
+			return `${Math.floor(seconds)} second${Math.floor(seconds) > 1 ? 's' : ''} ago`;
 		}
 
 		return "now";
