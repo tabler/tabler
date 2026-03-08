56750e3fac4743abc75f9eaa7079d164cee69c3c
codecalm <codecalm@gmail.com>
2026-03-08 20:56:03 +0100

    feat: vendor Bootstrap 5.3.8 JavaScript sources into core

--- core/js/src/bootstrap.ts
+++ core/js/src/bootstrap.ts
@@ -1,7 +1,42 @@
 export * as Popper from '@popperjs/core'
 
-// Export all Bootstrap components directly for consistent usage
-export { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip } from 'bootstrap'
+export { default as Alert } from './bootstrap/alert.js'
+export { default as Button } from './bootstrap/button.js'
+export { default as Carousel } from './bootstrap/carousel.js'
+export { default as Collapse } from './bootstrap/collapse.js'
+export { default as Dropdown } from './bootstrap/dropdown.js'
+export { default as Modal } from './bootstrap/modal.js'
+export { default as Offcanvas } from './bootstrap/offcanvas.js'
+export { default as Popover } from './bootstrap/popover.js'
+export { default as ScrollSpy } from './bootstrap/scrollspy.js'
+export { default as Tab } from './bootstrap/tab.js'
+export { default as Toast } from './bootstrap/toast.js'
+export { default as Tooltip } from './bootstrap/tooltip.js'
 
-// Re-export everything as namespace for backward compatibility
-export * as bootstrap from 'bootstrap'
+import Alert from './bootstrap/alert.js'
+import Button from './bootstrap/button.js'
+import Carousel from './bootstrap/carousel.js'
+import Collapse from './bootstrap/collapse.js'
+import Dropdown from './bootstrap/dropdown.js'
+import Modal from './bootstrap/modal.js'
+import Offcanvas from './bootstrap/offcanvas.js'
+import Popover from './bootstrap/popover.js'
+import ScrollSpy from './bootstrap/scrollspy.js'
+import Tab from './bootstrap/tab.js'
+import Toast from './bootstrap/toast.js'
+import Tooltip from './bootstrap/tooltip.js'
+
+export const bootstrap = {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
