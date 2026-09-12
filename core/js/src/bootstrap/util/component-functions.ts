a63f263c331710e6e62029fa0f4e4cd6b0a2462a
codecalm <codecalm@gmail.com>
2026-09-13 00:37:27 +0200

    Add bootstrap-component skill and eventActionOnPlugin helper

--- core/js/src/bootstrap/util/component-functions.ts
+++ core/js/src/bootstrap/util/component-functions.ts
@@ -9,6 +9,22 @@ import EventHandler from '../dom/event-handler.js'
 import SelectorEngine from '../dom/selector-engine.js'
 import { isDisabled } from './index'
 
+interface PluginComponent {
+  NAME: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getOrCreateInstance(element: HTMLElement | string | null): any
+}
+
+interface EventActionData {
+  targets: HTMLElement[]
+  event: Event
+}
+
+interface PluginEventActionData extends EventActionData {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  instances: any[]
+}
+
 interface DismissibleComponent {
   EVENT_KEY: string
   NAME: string
@@ -36,4 +52,33 @@ const enableDismissTrigger = (component: DismissibleComponent, method = 'hide'):
   })
 }
 
-export { enableDismissTrigger }
+const eventAction = (onEvent: string, stringSelector: string, callback: (data: EventActionData) => void): void => {
+  const selector = `${stringSelector}:not(.disabled):not(:disabled)`
+
+  EventHandler.on(document, onEvent, selector, function (this: HTMLElement, event: Event) {
+    if (['A', 'AREA'].includes(this.tagName)) {
+      event.preventDefault()
+    }
+
+    const targetSelector = SelectorEngine.getSelectorFromElement(this)
+    const targets = targetSelector ? SelectorEngine.find(targetSelector) : [this]
+
+    callback({ targets, event })
+  })
+}
+
+const eventActionOnPlugin = (Plugin: PluginComponent, onEvent: string, stringSelector: string, method: string, callback: ((data: PluginEventActionData) => void) | null = null): void => {
+  eventAction(`${onEvent}.${Plugin.NAME}`, stringSelector, (data) => {
+    const instances = data.targets.filter(Boolean).map((element) => Plugin.getOrCreateInstance(element))
+
+    if (typeof callback === 'function') {
+      callback({ ...data, instances })
+    }
+
+    for (const instance of instances) {
+      instance[method]()
+    }
+  })
+}
+
+export { enableDismissTrigger, eventAction, eventActionOnPlugin }
