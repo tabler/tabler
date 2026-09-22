a50c9ab156c8c8aa9ff8133f5e8853f75812c2ad
Bartosz-Do <xbartoszdobija@gmail.com>
2026-09-22 21:45:30 +0200

    Add the Tree component for nested folders, links and checkboxes (#3109)
    Co-authored-by: codecalm <codecalm@gmail.com>

--- core/js/tabler.ts
+++ core/js/tabler.ts
@@ -15,6 +15,7 @@ import './src/sortable'
 import './src/otp-input'
 import './src/sparkline'
 import './src/strength'
+import './src/tree'
 
 // Re-export everything from bootstrap.ts (single source of truth)
 export * from './src/bootstrap'
@@ -31,6 +32,7 @@ export { default as Sortable } from './src/sortable'
 export { default as Sparkline } from './src/sparkline'
 export { default as Strength } from './src/strength'
 export { default as SwitchIcon } from './src/switch-icon'
+export { default as Tree } from './src/tree'
 
 // deprecated(2.0): the `tabler` namespace with `getColor()`, `hexToRgba()` and `prefix`
 export * as tabler from './src/deprecated'
